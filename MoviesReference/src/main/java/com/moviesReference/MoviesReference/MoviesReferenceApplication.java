0f8073f46b1f3aef9c4fc8eda891c6d7b0da2e4d
e154389 <e154389@E-0611.univ-lemans.fr>
2019-01-07 17:52:41 +0100

    1er commit

---
+++ MoviesReference/src/main/java/com/moviesReference/MoviesReference/MoviesReferenceApplication.java
@@ -0,0 +1,14 @@
+package com.moviesReference.MoviesReference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesReferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesReferenceApplication.class, args);
+	}
+
+}
+
--- README.md
+++
@@ -1,1 +0,0 @@
-# MoviesReference
